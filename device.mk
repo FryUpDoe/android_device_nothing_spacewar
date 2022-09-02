1dc399a4b9a1ee7c74d22634a1a21f11ac5ec91e
Aaron Doe <110971929+FryUpDoe@users.noreply.github.com>
2022-09-02 17:31:30 +0100

    spacewar: bring in complete rootdir
    Co-Authored-By: David Kang <3746349+hahagu@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -121,6 +121,50 @@ PRODUCT_COPY_FILES += \
 # Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+    
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.kernel.post_boot-lahaina.sh \
+    init.kernel.post_boot-shima.sh \
+    init.kernel.post_boot-yupik.sh \
+    init.kernel.post_boot.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.display_boot.sh \
+    init.qti.kernel.debug-lahaina.sh \
+    init.qti.kernel.debug-shima.sh \
+    init.qti.kernel.debug-yupik.sh \
+    init.qti.kernel.debug.sh \
+    init.qti.kernel.sh \
+    init.qti.keymaster.sh \
+    init.qti.media.sh \
+    init.qti.qcv.sh \
+    qca6234-service.sh \
+    vendor_modprobe.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.default \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qti.kernel.rc \
+    init.qti.ufs.rc \
+    init.target.rc \
+    init.recovery.qcom.rc \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.default
 
 # fastbootd
 PRODUCT_PACKAGES += \
